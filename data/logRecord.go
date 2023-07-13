75ca4f8d9c9fc641202be7cc8999904fe8f05d34
galacy <galacy@galacydeMacBook-Air.local>
2023-07-13 11:22:37 +0800

    finish the encode logRecord function, decode logRecord header function and get logRecord's CRC function

--- data/logRecord.go
+++ data/logRecord.go
@@ -1,8 +1,11 @@
 package data
 
-import "encoding/binary"
+import (
+	"encoding/binary"
+	"hash/crc32"
+)
 
-type LogRecordType byte
+type LogRecordType = byte
 
 const (
 	LogRecordNormal LogRecordType = iota
@@ -37,17 +40,84 @@ type LogRecordPos struct {
 	Offset int64  //数据存储在文件中的哪个位置
 }
 
-// EncodeLogRecord Encode LogRecord, return a byte arrary and length
+// EncodeLogRecord Encode LogRecord, return a byte array and length
+//
+//		4 bytes    1byte    variant(max 5)	variant(max 5)
+//	-----------+----------+--------------+----------------+-----------+-----------+
+//	|  crc   |    type   |	  key size  |	 value size   |	    key    |	value  |
+//	----------+--------- +--------------+-----------------+----------+-----------+
 func EncodeLogRecord(logRecord *LogRecord) ([]byte, int64) {
-	return nil, 0
+	//construct a header byte array
+	header := make([]byte, maxLogRecordHeaderSize)
+
+	//save type value after 4 bytes(crc size)
+	header[4] = logRecord.Type
+	var index = 5
+
+	//save the key size and value size after 5 bytes
+	//use variant type
+	//update the index after every save operation
+	index += binary.PutVarint(header[index:], int64(len(logRecord.Key)))
+	index += binary.PutVarint(header[index:], int64(len(logRecord.Value)))
+
+	//get the size of encoded bytes array
+	var encoBytesSize = index + len(logRecord.Key) + len(logRecord.Value)
+
+	//construct the encoded logRecord bytes array
+	encoBytes := make([]byte, encoBytesSize)
+
+	//copy the header part
+	copy(encoBytes[:index], header[:index])
+	//save logRecord's key and value, because key and value is already the byte form, we don't need to encode them
+	copy(encoBytes[index:], logRecord.Key)
+	copy(encoBytes[index+len(logRecord.Key):], logRecord.Value)
+
+	//calculate crc value using the whole bytes array expects crc
+	crc := crc32.ChecksumIEEE(encoBytes[4:])          //return uint32
+	binary.LittleEndian.PutUint32(encoBytes[:4], crc) //PutUint32: take 4 bytes place
+
+	//for testing
+	//fmt.Printf("header length:%d, crc: %d\n", index, crc)
+
+	return encoBytes, int64(encoBytesSize)
 }
 
 // decode the header information in the byte array
 // return logRecordHeader and it's size
 func decodeLogRecordHeader(buf []byte) (*logRecordHeader, int64) {
-	return nil, 0
+	if len(buf) < 4 {
+		return nil, 0
+	}
+
+	header := &logRecordHeader{
+		crc:           binary.LittleEndian.Uint32(buf[:4]),
+		logRecordType: buf[4],
+	}
+
+	var index = 5
+	//get the keySize
+	keySize, n := binary.Varint(buf[index:])
+	header.keySize = uint32(keySize)
+	index += n
+
+	//get the value size
+	valueSize, n := binary.Varint(buf[index:])
+	header.valueSize = uint32(valueSize)
+	index += n //index is the size of logRecordHeader now
+
+	return header, int64(index)
 }
 
 func getLogRecordCRC(logRecord *LogRecord, header []byte) uint32 {
-	return 0
+	if logRecord == nil {
+		return 0
+	}
+
+	//calculate the crc using header data
+	crc := crc32.ChecksumIEEE(header[:])
+	//update the crc using logRecord's key and value
+	crc = crc32.Update(crc, crc32.IEEETable, logRecord.Key)
+	crc = crc32.Update(crc, crc32.IEEETable, logRecord.Value)
+
+	return crc
 }
