fe0515a14db26a45ef60817e2b63acdcde6b99ad
galacy <galacy@galacydeMacBook-Air.local>
2023-07-13 11:36:35 +0800

    generate random key and value for testing

---
+++ utils/rand_kv.go
@@ -0,0 +1,26 @@
+package utils
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+var (
+	randStr = rand.New(rand.NewSource(time.Now().Unix()))
+	letters = []byte("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+)
+
+// GetTestKey get key for testing
+func GetTestKey(i int) []byte {
+	return []byte(fmt.Sprintf("bitcask-go-key-%09d", i))
+}
+
+// RandomValue n means the size of value, construct random value for testing
+func RandomValue(n int) []byte {
+	b := make([]byte, n)
+	for i := range b {
+		b[i] = letters[randStr.Intn(len(letters))]
+	}
+	return []byte("bitcask-go-value-" + string(b))
+}
