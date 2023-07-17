f5b57f9665fe811208f6798cd6195cc1f833f3a5
galacy <galacy@galacydeMacBook-Air.local>
2023-07-17 15:29:30 +0800

    add a Iterator interface, add a Iterator function in Indexer interface

--- index/index.go
+++ index/index.go
@@ -17,9 +17,16 @@ type Indexer interface {
 	// Get : get the position information by key
 	Get(key []byte) *data.LogRecordPos //通过key拿到索引位置信息
 
-	// Delete  delete the position information by key
+	// Delete the position information by key
 	//通过key删除对应的索引位置信息
+
 	Delete(key []byte) bool
+
+	// Size return index's size
+	Size() int
+
+	// Iterator index iterator
+	Iterator(reverse bool) Iterator
 }
 
 // IndexType enum different type of indexers
@@ -55,4 +62,22 @@ func (aitem Item) Less(bitem btree.Item) bool {
 
 	return bytes.Compare(aitem.key, bitem.(*Item).key) == -1
 
+}
+
+// Iterator generic index iterator interface
+type Iterator interface {
+	// Rewind return to the beginning of iterator
+	Rewind()
+	// Seek find the first key that is greater than or equal to the target key, and iterate from there
+	Seek(key []byte)
+	// Next jump to next key
+	Next()
+	// Valid check if the key is available, in other words, check if the iterate is over, in order to quit to iterate
+	Valid() bool
+	// Key get the key in current iterate position
+	Key() []byte
+	// Value get the values in current iterate position
+	Value() *data.LogRecordPos
+	// Close close the current iterator and release relevant resources
+	Close()
 }
