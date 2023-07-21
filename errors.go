f3fa79bf39d64fa08c2d511ecabb71adc180f29b
galacy <galacy@galacydeMacBook-Air.local>
2023-07-21 14:26:17 +0800

    add a ExceedMaxBatchNum error and MergeIsProcessing error

--- errors.go
+++ errors.go
@@ -9,4 +9,6 @@ var (
 	ErrDataFileNotFound       = errors.New("data file is not found")
 	ErrLogRecordDeleted       = errors.New("logrecord has been deleted")
 	ErrDataDirectoryCorrupted = errors.New("the data directory may be corrupted")
+	ErrExceedMaxBatchNum      = errors.New("exceed the max batch number")
+	ErrMergeIsProcessing      = errors.New("merge is in the process, try again later")
 )
