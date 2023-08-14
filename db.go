cf97bc506e47084478658d4541c69e3f278f28e3
galacy <galacy0420@gmail.com>
2023-08-14 10:10:27 +0800

    make sure only one process can use db by adding filelock add Stat function to stat some information in db add Backup function to make a backup update Close function:save the current seqNo and close the indexer update reclaimSize in Delete and put function do sync when byteWrite satisfied BytesPerSync open data file as MMap depend on user's option add loadSeqNo and resetIOType function

--- db.go
+++ db.go
@@ -2,8 +2,12 @@ package bitcaskGo
 
 import (
 	"bitcaskGo/data"
+	"bitcaskGo/fileio"
 	"bitcaskGo/index"
+	"bitcaskGo/utils"
 	"errors"
+	"fmt"
+	"github.com/gofrs/flock"
 	"io"
 	"os"
 	"path/filepath"
@@ -13,15 +17,32 @@ import (
 	"sync"
 )
 
+const (
+	seqNoKey     = "seq-no"
+	fileLockName = "flock"
+)
+
 type DB struct {
-	options    Options
-	mu         *sync.RWMutex
-	activeFile *data.Datafile            //current active file  use for write
-	olderFiles map[uint32]*data.Datafile //the set of older file map by fileid(uint32) only for read
-	index      index.Indexer             //Memory index 内存索引
-	fileIds    []int                     //File id,only use for load index, can't be used or update in other place
-	seqNo      uint64                    //the transaction sequence number, increase globally
-	isMerging  bool                      //check if the database is in the process of merging
+	options         Options
+	mu              *sync.RWMutex
+	activeFile      *data.Datafile            //current active file  use for write
+	olderFiles      map[uint32]*data.Datafile //the set of older file map by fileid(uint32) only for read
+	index           index.Indexer             //Memory index 内存索引
+	fileIds         []int                     //File id,only use for load index, can't be used or update in other place
+	seqNo           uint64                    //the transaction sequence number, increase globally
+	isMerging       bool                      //check if the database is in the process of merging
+	seqNoFileExists bool                      //signify that if the file which save the transaction seqNo exists
+	isInitial       bool                      //if is the first time to initial this data directory
+	fileLock        *flock.Flock              //file lock ensure the mutex of different processes
+	bytesWrite      uint                      //the total number of bytes that were written
+	reclaimSize     int64                     //signify the size that need to be merged/reclaimed
+}
+
+type Stat struct {
+	KeyNum          uint  //number of keys in database
+	DataFileNum     uint  //number of data files
+	ReclaimableSize int64 //number of data that can be merged (in bytes)
+	DiskSize        int64 //Disk space occupied by the data directory
 }
 
 // Open Open a Bitcask storage engine instance.
@@ -31,17 +52,42 @@ func Open(options Options) (*DB, error) {
 	if err := CheckOptions(options); err != nil {
 		return nil, err
 	}
+	var isInitial bool
 	//Check if the data directory exist, if not, create a new one
 	if _, err := os.Stat(options.DirPath); os.IsNotExist(err) {
+		isInitial = true
 		if err := os.MkdirAll(options.DirPath, os.ModePerm); err != nil {
 			return nil, err
 		}
 	}
+
+	//check if the current data directory is using
+	fileLock := flock.New(filepath.Join(options.DirPath, fileLockName))
+	hold, err := fileLock.TryLock()
+	if err != nil {
+		return nil, err
+	}
+	if !hold {
+		return nil, ErrDataBaseisUsing
+	}
+
+	//if the data directory exists, but it's empty,
+	//we still need to set isInitial to true
+	entries, err := os.ReadDir(options.DirPath)
+	if err != nil {
+		return nil, err
+	}
+	if len(entries) == 0 {
+		isInitial = true
+	}
+
 	db := &DB{
 		options:    options,
 		mu:         new(sync.RWMutex),
 		olderFiles: make(map[uint32]*data.Datafile),
-		index:      index.NewIndexer(options.IndexerType),
+		index:      index.NewIndexer(options.IndexerType, options.DirPath, options.SyncWrites),
+		isInitial:  isInitial,
+		fileLock:   fileLock,
 	}
 
 	//load merge data directory
@@ -51,18 +97,74 @@ func Open(options Options) (*DB, error) {
 
 	//Load the data file
 	if err := db.loadDataFiles(); err != nil {
+
 		return nil, err
 	}
 
-	//load index from hint file
-	if err := db.loadIndexFromHintFile(); err != nil {
-		return nil, err
+	//if we use b plus tree as the indexer
+	//we don't need to load index from data files
+	if options.IndexerType != BPTree {
+		//load index from hint file
+		if err := db.loadIndexFromHintFile(); err != nil {
+			return nil, err
+		}
+
+		//load index from data files
+		if err := db.loadIndexFromDataFiles(); err != nil {
+			return nil, err
+		}
+		//reset the ioType to standard file io
+		if err := db.resetIOType(); err != nil {
+			return nil, err
+		}
+
 	}
 
-	if err := db.loadIndexFromDataFiles(); err != nil {
-		return nil, err
+	//retrieve the current seqNo when indexer is B Plus Tree
+	if options.IndexerType == BPTree {
+		if err := db.loadSeqNo(); err != nil {
+			return nil, err
+		}
+		//because we jump out the loadIndex function,
+		//thus we need to update the writeoff in active file manually
+		if db.activeFile != nil {
+			size, err := db.activeFile.IOManager.Size()
+			if err != nil {
+				return nil, err
+			}
+			db.activeFile.WriteOff = size
+		}
 	}
 	return db, nil
+}
+
+// Stat return relevant stat information of database
+func (db *DB) Stat() *Stat {
+	db.mu.RLock()
+	defer db.mu.RUnlock()
+	var dataFileNum = uint(len(db.olderFiles))
+	if db.activeFile != nil {
+		dataFileNum += 1
+	}
+
+	dirSize, err := utils.DirSize(db.options.DirPath)
+	if err != nil {
+		panic(fmt.Sprintf("failed to get dir size : %v", err))
+	}
+
+	return &Stat{
+		KeyNum:          uint(db.index.Size()),
+		DataFileNum:     dataFileNum,
+		ReclaimableSize: db.reclaimSize,
+		DiskSize:        dirSize, //
+	}
+}
+
+// Backup the database, copy all the data file to new directory
+func (db *DB) Backup(dir string) error {
+	db.mu.RLock()
+	defer db.mu.RUnlock()
+	return utils.CopyDir(db.options.DirPath, dir, []string{fileLockName})
 }
 
 // Put Write key/value data , the key can't be empty
@@ -84,8 +186,8 @@ func (db *DB) Put(key []byte, value []byte) error {
 		return err
 	}
 
-	if success := db.index.Put(key, pos); !success {
-		return ErrIndexUpdateFailed
+	if oldPos := db.index.Put(key, pos); oldPos != nil {
+		db.reclaimSize += int64(oldPos.Size)
 	}
 
 	return nil
@@ -115,12 +217,41 @@ func (db *DB) Get(key []byte) ([]byte, error) {
 
 // Close the database
 func (db *DB) Close() error {
+	defer func() {
+		if err := db.fileLock.Unlock(); err != nil {
+			panic(fmt.Sprintf("failed to unlock the directory, %v", err))
+		}
+
+		//when the indexer is bptree, we need to close the indexer when close the db
+		//because the indexer is also a db
+		if err := db.index.Close(); err != nil {
+			panic(fmt.Sprintf("failed to close the index"))
+		}
+	}()
 	if db.activeFile == nil {
 		return nil
 	}
 
 	db.mu.Lock()
 	defer db.mu.Unlock()
+
+	//save the current seq no
+	seqNoFile, err := data.OpenSeqNoFile(db.options.DirPath)
+	if err != nil {
+		return err
+	}
+	seqNoRecord := &data.LogRecord{
+		Key:   []byte(seqNoKey),
+		Value: []byte(strconv.FormatUint(db.seqNo, 10)), //converse the seqNo into a decimal string
+	}
+	encLogRecord, _ := data.EncodeLogRecord(seqNoRecord)
+	if err := seqNoFile.Write(encLogRecord); err != nil {
+		return err
+	}
+
+	if err := seqNoFile.Sync(); err != nil {
+		return err
+	}
 
 	//close the active data file
 	if err := db.activeFile.Close(); err != nil {
@@ -149,6 +280,7 @@ func (db *DB) Sync() error {
 // ListKeys get all keys in the database
 func (db *DB) ListKeys() [][]byte {
 	iterator := db.index.Iterator(false) //use index iterator, because index have all keys' information
+	defer iterator.Close()
 	keys := make([][]byte, db.index.Size())
 	var idx int
 	for iterator.Rewind(); iterator.Valid(); iterator.Next() {
@@ -163,7 +295,9 @@ func (db *DB) ListKeys() [][]byte {
 func (db *DB) Fold(fn func(key []byte, value []byte) bool) error {
 	db.mu.RLock()
 	defer db.mu.RUnlock()
+
 	iterator := db.index.Iterator(false)
+	defer iterator.Close()
 	for iterator.Rewind(); iterator.Valid(); iterator.Next() {
 		value, err := db.getValueByPosition(iterator.Value())
 		if err != nil {
@@ -225,13 +359,18 @@ func (db *DB) Delete(key []byte) error {
 	}
 
 	//Write(append) this logRecord to data file
-	_, err := db.appendLogRecordWithLock(logRecord)
+	pos, err := db.appendLogRecordWithLock(logRecord)
 	if err != nil {
 		return err
 	}
+	db.reclaimSize += int64(pos.Size)
 	//Delete the correspond key in index
-	if success := db.index.Delete(key); !success {
+	oldPos, success := db.index.Delete(key)
+	if !success {
 		return ErrIndexUpdateFailed
+	}
+	if oldPos != nil {
+		db.reclaimSize += int64(oldPos.Size)
 	}
 	return nil
 }
@@ -280,16 +419,27 @@ func (db *DB) appendLogRecord(logRecord *data.LogRecord) (*data.LogRecordPos, er
 		return nil, err
 	}
 
+	db.bytesWrite += uint(length)
+
 	//Do the sync() options depends on user's option
-	if db.options.SyncWrites {
+	var needSync = db.options.SyncWrites
+	if !needSync && db.options.BytesPerSync > 0 && db.bytesWrite >= db.options.BytesPerSync {
+		needSync = true
+	}
+	if needSync {
 		if err := db.activeFile.Sync(); err != nil {
 			return nil, err
+		}
+
+		//reset the bytesWrite
+		if db.bytesWrite > 0 {
+			db.bytesWrite = 0
 		}
 	}
 
 	//Construct memory index information
 	//构造内存索引信息
-	pos := &data.LogRecordPos{FileId: db.activeFile.Fileid, Offset: writeoff}
+	pos := &data.LogRecordPos{FileId: db.activeFile.Fileid, Offset: writeoff, Size: uint32(length)}
 	return pos, nil
 
 }
@@ -303,7 +453,7 @@ func (db *DB) setActiveDataFile() error {
 		//the new active datafile's FileId should plus 1
 		initialFileId = db.activeFile.Fileid + 1
 	}
-	dataFile, err := data.OpenDataFile(db.options.DirPath, initialFileId)
+	dataFile, err := data.OpenDataFile(db.options.DirPath, initialFileId, fileio.StandardFIO)
 	if err != nil {
 		return err
 	}
@@ -335,7 +485,11 @@ func (db *DB) loadDataFiles() error {
 
 	//Go through each file id and open the correspond data file
 	for i, fid := range fileIds {
-		dataFile, err := data.OpenDataFile(db.options.DirPath, uint32(fid))
+		ioType := fileio.StandardFIO
+		if db.options.MMapAtStartup {
+			ioType = fileio.MemoryMap
+		}
+		dataFile, err := data.OpenDataFile(db.options.DirPath, uint32(fid), ioType)
 		if err != nil {
 			return err
 		}
@@ -371,14 +525,15 @@ func (db *DB) loadIndexFromDataFiles() error {
 	}
 	updateIndex := func(key []byte, typ data.LogRecordType, pos *data.LogRecordPos) {
 		//Check if the logRecord has been deleted
-		var success bool
+		var oldPos *data.LogRecordPos
 		if typ == data.LogRecordDeleted {
-			success = db.index.Delete(key)
+			oldPos, _ = db.index.Delete(key)
+			db.reclaimSize += int64(pos.Size)
 		} else { //Save the key---logRecordPos index to memory indexer
-			success = db.index.Put(key, pos)
+			oldPos = db.index.Put(key, pos)
 		}
-		if !success {
-			panic("failed to update index when startup")
+		if oldPos != nil {
+			db.reclaimSize += int64(oldPos.Size)
 		}
 	}
 
@@ -408,7 +563,7 @@ func (db *DB) loadIndexFromDataFiles() error {
 		var offset int64 = 0 //In each datafile, the offset stars from 0
 		for {
 			logRecord, size, err := dataFile.ReadLogRecord(offset)
-			//Check the type of err: when we hit the last record, it's correct to get a EOF err
+			//Check the type of err: when we hit the last record, it's correct to get an EOF err
 			if err != nil {
 				if err == io.EOF {
 					break //jump out of the loop
@@ -417,7 +572,7 @@ func (db *DB) loadIndexFromDataFiles() error {
 			}
 
 			//Construct and save the memory index
-			logRecordPos := &data.LogRecordPos{FileId: fileid, Offset: offset}
+			logRecordPos := &data.LogRecordPos{FileId: fileid, Offset: offset, Size: uint32(size)}
 
 			//parse the key, get the real key and seqNo
 			realKey, seqNo := parselogRecordKey(logRecord.Key)
@@ -456,7 +611,7 @@ func (db *DB) loadIndexFromDataFiles() error {
 			//Update the offset , read in a new position next time
 			offset += size
 		}
-		//If we are in the last(active) file, we need to update the active file's WriteOff by using offset
+		//If we are in the active(last) file, we need to update the active file's WriteOff by using offset
 		//make sure that we can append the logRecord in a right position
 		if i == len(db.fileIds)-1 {
 			db.activeFile.WriteOff = offset
@@ -477,5 +632,50 @@ func CheckOptions(options Options) error {
 		return errors.New("database data file size must be greater than 0")
 	}
 
+	if options.DataFileMergeRatio < 0 || options.DataFileMergeRatio > 1 {
+		return errors.New("invalid merge ratio, must between 0 and 1")
+	}
+
+	return nil
+}
+
+func (db *DB) loadSeqNo() error {
+	fileName := filepath.Join(db.options.DirPath, data.SeqNoFileName)
+	if _, err := os.Stat(fileName); os.IsNotExist(err) {
+		return nil
+	}
+	seqNofile, err := data.OpenSeqNoFile(db.options.DirPath)
+	if err != nil {
+		return err
+	}
+
+	record, _, err := seqNofile.ReadLogRecord(0)
+	seqNo, err := strconv.ParseUint(string(record.Value), 10, 64)
+	if err != nil {
+		return err
+	}
+
+	db.seqNoFileExists = true
+	db.seqNo = seqNo
+	return os.Remove(fileName)
+}
+
+// set data file's ioType to standard file io
+func (db *DB) resetIOType() error {
+	if db.activeFile == nil {
+		return nil
+	}
+
+	//reset the current active data file
+	if err := db.activeFile.SetIOManager(db.options.DirPath, fileio.StandardFIO); err != nil {
+		return err
+	}
+
+	//reset old data files
+	for _, datafile := range db.olderFiles {
+		if err := datafile.SetIOManager(db.options.DirPath, fileio.StandardFIO); err != nil {
+			return err
+		}
+	}
 	return nil
 }
